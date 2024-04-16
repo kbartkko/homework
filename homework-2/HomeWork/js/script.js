e0e1ab8b1bcf8b1473edf652f98983b9e892b479
kbartkko <146842563+kbartkko@users.noreply.github.com>
2024-04-16 19:01:07 +0200

    Add files via upload

---
+++ homework-2/HomeWork/js/script.js
@@ -0,0 +1,172 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+// let num = 1;
+// (num > 0) ? console.log(true) : console.log(false);
+
+// num = 0;
+// (num > 0) ? console.log(true) : console.log(false);
+
+// num = -3;
+// (num > 0) ? console.log(true) : console.log(false);
+
+
+// (1 > 0) ? console.log("true") : console.log("false");
+// (0 > 0) ? console.log("true") : console.log("false");
+// (-3 > 1) ? console.log("true") : console.log("false");
+// (5 > 0) ? console.log("true") : console.log("false")
+ 
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// let value = "true"; 
+// switch (value) {
+//     case "test":
+//         console.log(true);
+//         break;
+//     case "qwerty":
+//         console.log(false);
+//         break;
+//     case "true":
+//         console.log(false);
+//         break;
+    
+//     default: console.log("enter another value")
+  
+// }
+
+
+// switch ('true') {
+//     case 'test':
+//         console.log(true);
+//         break;
+//     case 'gwerty':
+//         console.log(true);
+//         break;
+//     case 'true':
+//         console.log(true);
+//         break;
+//     default: console.log(false);
+// }
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+
+
+// let num = Number(prompt("Enter a number"))
+
+// if (num > 10) {
+//     console.log(num - 5)
+// } else if(num < 10) {
+//     console.log(num + 5)
+// }
+
+
+
+// let a = Number(prompt("Введіть число"));
+// if (a >= 10) {
+//     console.log(a - 5);
+    
+// } else if (a <= 10) {
+//     console.log(a + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const month = Number(prompt("Enter number 1 to 12"))
+
+// switch (month) {
+//     case 1:
+//         alert(`${month} is January!`);
+//         break;
+//     case 2:
+//         alert(`${month} is February!`);
+//         break;
+//     case 3:
+//         alert(`${month} is March!`);
+//         break;
+//     case 4:
+//         alert(`${month} is April!`);
+//         break;
+//     case 5:
+//         alert(`${month} is May!`);
+//         break;
+//     case 6:
+//         alert(`${month} is June!`);
+//         break;
+//     case 7:
+//         alert(`${month} is Jule!`);
+//         break;
+//     case 8:
+//         alert(`${month} is August!`);
+//         break;
+//     case 9:
+//         alert(`${month} is September!`);
+//         break;
+//     case 10:
+//         alert(`${month} is November!`);
+//         break;
+//     case 11:
+//         alert(`${month} is October!`);
+//         break;
+//     case 12:
+//         alert(`${month} is December!`);
+//         break;
+//     default: alert("Enter another number from 1 to 12!")
+// }
+
+
+
+
+// // const month = prompt("Введіть число від 1 до 12");
+
+// // switch (month) {
+// //     case ('1'):
+// //         alert(`${month} це Січень`);
+// //         break;
+// //      case ('2'):
+// //         alert(`${month} це Лютий`);
+// //         break;
+// //      case ('3'):
+// //         alert(`${month} це Березень`);
+// //         break;
+// //      case ('4'):
+// //         alert(`${month} це Квітень`);
+// //         break;
+// //      case ('5'):
+// //         alert(`${month} це Травень`);
+// //         break;
+// //      case ('6'):
+// //         alert(`${month} це Червень`);
+// //         break;
+// //      case ('7'):
+// //         alert(`${month} це Липень`);
+// //         break;
+// //      case ('8'):
+// //         alert(`${month} це Серпень`);
+// //         break;
+// //      case ('9'):
+// //         alert(`${month} це Вересень`);
+// //         break;
+// //      case ('10'):
+// //         alert(`${month} це Жовтень`);
+// //         break;
+// //      case ('11'):
+// //         alert(`${month} це Листопад`);
+// //         break;
+// //      case ('12'):
+// //         alert(`${month} це Грудень`);
+// //         break;
+// //     default: alert(`Немає такого місяця!`);
+// // }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+let num = parseInt(prompt("Enter 3 number"))
+
+
+
+
